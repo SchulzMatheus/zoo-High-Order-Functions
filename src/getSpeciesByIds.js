90f816ccfbbc9cf36875c5fc0f11be2b0975fce6
Matheus Simplicio <matheusschulz47@gmail.com>
2022-09-29 18:52:01 -0300

    removendo o console.log de getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,6 +1,4 @@
 const { species } = require('../data/zoo_data');
 
 const getSpeciesByIds = (...ids) => species.filter(({ id }) => ids.includes(id));
-
-console.log(getSpeciesByIds('01422318-ca2d-46b8-b66c-3e9e188244ed'));
 module.exports = getSpeciesByIds;

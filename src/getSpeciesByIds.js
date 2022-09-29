c2429cd85f6bf73342fa77a3f0fbdcfa54b4e494
Matheus Simplicio <matheusschulz47@gmail.com>
2022-09-29 18:50:45 -0300

    Assim tá melhor

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,9 +1,6 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-const getSpeciesByIds = (...ids) => {
-  const array = [];
-  if (ids.length !== 0) ids.forEach((id) => array.push(data.species.find((ele) => ele.id === id)));
-  return array;
-};
+const getSpeciesByIds = (...ids) => species.filter(({ id }) => ids.includes(id));
+
 console.log(getSpeciesByIds('01422318-ca2d-46b8-b66c-3e9e188244ed'));
 module.exports = getSpeciesByIds;

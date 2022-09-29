1ac9d3b10ac4ca334bc4658df635235a41725347
Matheus Simplicio <matheusschulz47@gmail.com>
2022-09-29 18:13:16 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
-
+const getSpeciesByIds = (...ids) => {
+  const array = [];
+  if (ids.length !== 0) ids.forEach((id) => array.push(data.species.find((ele) => ele.id === id)));
+  return array;
+};
+console.log(getSpeciesByIds('01422318-ca2d-46b8-b66c-3e9e188244ed'));
 module.exports = getSpeciesByIds;

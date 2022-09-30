91818cdf82c4f76300b84cdc72ff68985adb2a7a
Matheus Simplicio <matheusschulz47@gmail.com>
2022-09-30 16:51:31 -0300

    "enxugando" o código

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -3,12 +3,10 @@ const { employees } = require('../data/zoo_data');
 const isManager = (id) => employees.some((emp) => emp.managers.includes(id));
 
 const getRelatedEmployees = (managerId) => {
-  const newArray = [];
   if (isManager(managerId)) {
-    employees.forEach((e) => {
-      if (e.managers.includes(managerId)) newArray.push(`${e.firstName} ${e.lastName}`);
-    });
-    return newArray;
+    return employees
+      .filter((ele) => ele.managers.includes(managerId))
+      .map((ele) => `${ele.firstName} ${ele.lastName}`);
   } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 };
 

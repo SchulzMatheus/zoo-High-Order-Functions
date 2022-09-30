dc7f8ffbe36e2207eb8687fd65d07635fd04e1f0
Matheus Simplicio <matheusschulz47@gmail.com>
2022-09-30 17:01:19 -0300

    modificando getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,12 +2,13 @@ const { employees } = require('../data/zoo_data');
 
 const isManager = (id) => employees.some((emp) => emp.managers.includes(id));
 
+const filteredEmployers = (id) => (
+  employees.filter((e) => e.managers.includes(id)).map((e) => `${e.firstName} ${e.lastName}`)
+);
+
 const getRelatedEmployees = (managerId) => {
-  if (isManager(managerId)) {
-    return employees
-      .filter((ele) => ele.managers.includes(managerId))
-      .map((ele) => `${ele.firstName} ${ele.lastName}`);
-  } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  if (isManager(managerId)) return filteredEmployers(managerId);
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 };
 
 module.exports = { isManager, getRelatedEmployees };

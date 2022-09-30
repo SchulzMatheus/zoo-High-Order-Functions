97ff326cac9858bad0a1c1e325c30cb79ca8939d
Matheus Simplicio <matheusschulz47@gmail.com>
2022-09-29 22:52:12 -0300

    requirement 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,7 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
+const getAnimalsOlderThan = (
+  (animal, age) => species.find((s) => s.name === animal).residents.every((r) => r.age >= age)
+);
 
 module.exports = getAnimalsOlderThan;

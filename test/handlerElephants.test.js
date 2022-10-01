88b37cb2c2199d1e729b5d2e8ff0ed3f2ed3ed73
Matheus Simplicio <matheusschulz47@gmail.com>
2022-09-30 22:29:59 -0300

    requirement 7 and 15

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -19,4 +19,13 @@ describe('Testes da função HandlerElephants', () => {
   it('retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
     expect(handlerElephants('availability').includes(['Friday', 'Saturday', 'Sunday', 'Tuesday']));
   });
+  it('se passar um parametro vazio retorna null', () => {
+    expect(handlerElephants('')).toEqual(null);
+  });
+  it('se passar um parametro vazio retorna undefined', () => {
+    expect(handlerElephants(undefined)).toEqual(undefined);
+  });
+  it('se passar um parametro diferente de string retorna undefined', () => {
+    expect(handlerElephants(!String)).toBe('Parâmetro inválido, é necessário uma string');
+  });
 });

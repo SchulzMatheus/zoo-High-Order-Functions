00be32d4563462a14d8ea8f07dce40943e74e122
Matheus Simplicio <matheusschulz47@gmail.com>
2022-09-30 22:19:32 -0300

    requirement 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,22 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('retorna a quantidade de elefantes', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('retorna um array com a relação dos nomes de todos os elefantes', () => {
+    expect(handlerElephants('names').includes(['Jefferson', 'Ilana', 'Bea', 'Jefferson']));
+  });
+  it('retorna a média de idade dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toBe(10.5);
+  });
+  it('retorna a localização dos elefantes dentro do Zoológico', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  it('retorna a popularidade dos elefantes', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+  it('retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
+    expect(handlerElephants('availability').includes(['Friday', 'Saturday', 'Sunday', 'Tuesday']));
+  });
+});

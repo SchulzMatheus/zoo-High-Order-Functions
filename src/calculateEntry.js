a3d16bf607d5fa2c4d9a52e4239acbc58a914f78
Matheus Simplicio <matheusschulz47@gmail.com>
2022-10-03 21:34:37 -0300

    requirement 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,36 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
-}
+const x = 0;
+
+const entrantsx = [
+  { name: 'marcio', age: 5 },
+  { name: 'marcio', age: 5 },
+  { name: 'marcio', age: 5 },
+  { name: 'marcio', age: 18 },
+  { name: 'marcio', age: 18 },
+  { name: 'marcio', age: 50 },
+];
+
+const ifIsnotNull = (array) => {
+  const obj = { adult: 0, child: 0, senior: 0 };
+  array.forEach((entrant) => {
+    if (entrant.age < 18) obj.child += 1;
+    else if (entrant.age >= 18 && entrant.age < 50) obj.adult += 1;
+    else obj.senior += 1;
+  });
+  return obj;
+};
+
+const countEntrants = (entrants) => ((entrants !== null) ? ifIsnotNull(entrants) : x);
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) return 0;
+  let totalValue = 0;
+  const newObj = countEntrants(entrants);
+  totalValue += (newObj.child * data.prices.child)
+  + (newObj.adult * data.prices.adult)
+  + (newObj.senior * data.prices.senior);
+  return totalValue;
 }
-
+console.log(countEntrants(entrantsx));
 module.exports = { calculateEntry, countEntrants };

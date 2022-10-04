c900e973e8dadf7f15765a8ec472e5a87940e328
Matheus Simplicio <matheusschulz47@gmail.com>
2022-10-04 00:42:46 -0300

    agora é só correr pro abraço

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -3,14 +3,10 @@ const data = require('../data/zoo_data');
 const { employees, species } = data;
 const getList = (id) => {
   const emp = employees.find((employee) => employee.id === id);
-  // return emp;
   const animalsList = species.find((specie) => specie.id === emp.responsibleFor[0]);
-  // return animalsList;
   const oldestSorOrder = animalsList.residents.sort((x1, x2) => x2.age - x1.age)[0];
   return oldestSorOrder;
 };
 const getOldestFromFirstSpecies = (id) => [getList(id).name, getList(id).sex, getList(id).age];
 
-console.log(getList('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
-console.log(getOldestFromFirstSpecies('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
 module.exports = getOldestFromFirstSpecies;
